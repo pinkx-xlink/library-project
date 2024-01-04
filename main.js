0d7580f659fac79198e33a82fef13c6902c962f3
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-01-04 15:28:30 -0500

    Run dispayInfo() on book1

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ function Book(title, author, pages){
     this.readStatus = function() {
         const input = prompt(`Have you read ${this.title} yet?`)
         console.log(`"Have you read ${this.title} yet?" ${input}.`);
-    }
+    };
     this.displayInfo = function() {
         console.log(`"${this.title}" by ${this.author} has ${this.pages} pages.`)
-    }
+    };
 }
 
 const book1 = new Book('The Well of Loneliness', 'Radclyffe Hall', 399);
 book1.readStatus();
+book1.displayInfo();
