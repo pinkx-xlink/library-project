82f746cafbf8f3cc9cc247d3f06f00a72911ab9d
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-01-04 15:30:15 -0500

    Comment out displayInfo

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ function Book(title, author, pages){
         const input = prompt(`Have you read ${this.title} yet?`)
         console.log(`"Have you read ${this.title} yet?" ${input}.`);
     };
-    this.displayInfo = function() {
-        console.log(`"${this.title}" by ${this.author} has ${this.pages} pages.`)
-    };
+    // this.displayInfo = function() {
+    //     console.log(`"${this.title}" by ${this.author} has ${this.pages} pages.`)
+    // };
 }
 
 const book1 = new Book('The Well of Loneliness', 'Radclyffe Hall', 399);
 book1.readStatus();
-book1.displayInfo();
+//book1.displayInfo();
