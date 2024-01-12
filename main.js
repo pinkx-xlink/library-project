fad789e1db40726fd989f57ad2c7901b3d6658eb
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-01-12 13:59:19 -0500

    Comment out lines 2-6

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const myLibrary = [1, 2, 3];
-const bookShelf = myLibrary.map(item => {
-  const container{};
-  container[item.titleInput] = item.titleInput;
-  container[item.authorInput] = item.authorInput;
-});
+// const bookShelf = myLibrary.map(item => {
+//   const container{};
+//   container[item.titleInput] = item.titleInput;
+//   container[item.authorInput] = item.authorInput;
+// });
  
 function Book (title, author, pages) {
   this.title = title;
